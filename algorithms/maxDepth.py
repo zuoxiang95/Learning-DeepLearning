c08f0dcdef7d0676129120b103ea12b8dee5fedf
Zuoxiang <zx_data@126.com>
2017-09-01 16:01:23 +0800

    no message

--- algorithms/maxDepth.py
+++ algorithms/maxDepth.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 '''
-<<<<<<< HEAD
-将一个整数中的数字进行颠倒，当颠倒后的整数溢出时，返回 0 (标记为 32 位整数)。
-给定 x = 123，返回 321
-给定 x = -123，返回 -321
-'''
-
-'''
-判断数字是否大于为32位整数，如果是，则返回0，不是，遍历数字的每一位，定义一个变量储存正负值，反转数字。
-=======
 给定一个二叉树，找出其最大深度。
 二叉树的深度为根节点到最远叶子节点的距离。
 '''
@@ -25,9 +16,6 @@ class TreeNode:
 
 def maxDepth(root):
     # write your code here
-<<<<<<< HEAD
-    pass
-=======
     if root == None:
         return 0
     leftDepth = maxDepth(root.left)
@@ -36,4 +24,3 @@ def maxDepth(root):
         return leftDepth + 1
     else:
         return rightDepth + 1
->>>>>>> origin/master
