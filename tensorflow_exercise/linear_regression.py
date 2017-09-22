7006b37fb88818f8addaf8e30d58003d0d1855fc
zuoxiang <zx_data@126.com>
2017-09-22 11:04:47 +0800

    linear regression in TensorFlow

--- tensorflow_exercise/linear_regression.py
+++ tensorflow_exercise/linear_regression.py
@@ -3,58 +3,80 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+rng = np.random
 
-
-plt.ion()
-# 生成数据
-n_observations = 100
-fig, ax = plt.subplots(1, 1)
-xs = np.linspace(-3, 3, n_observations)
-ys = 5 * xs + 4 + np.random.uniform(-0.3, 0.3, n_observations)
-ax.plot(xs, ys)
-
-plt.draw()
-fig.show()
-
-# 设置占位符
-X = tf.placeholder(tf.float32)
-Y = tf.placeholder(tf.float32)
-
-# 创建权重和偏移量
-w = tf.Variable(tf.random_normal([1]), name='weight')
-b = tf.Variable(tf.random_normal([1]), name='bias')
-# 设置计算输出方法
-Y_predict = tf.add(tf.multiply(X, w), b)
-
-# 定义损失函数
-loss = tf.reduce_sum(tf.pow(Y_predict - ys, 2)) / (n_observations - 1)
-
-# 学习率
+# Parameters
 learning_rate = 0.01
-# 设置优化方法：使用梯度下降优化损失
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss=loss)
+training_epochs = 1000
+display_step = 50
 
-# 设置迭代次数
-n_epochs = 1000
+# Training Data
+train_X = np.asarray([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167,
+                      7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1])
+train_Y = np.asarray([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221,
+                      2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3])
+n_samples = train_X.shape[0]
+
+# tf Graph Input
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
+
+# Set model weights
+W = tf.Variable(rng.randn(), name="weight")
+b = tf.Variable(rng.randn(), name="bias")
+
+# Construct a linear model
+pred = tf.add(tf.multiply(X, W), b)
+
+# Mean squared error
+cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
+# Gradient descent
+#  Note, minimize() knows to modify W and b because Variable objects are trainable=True by default
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# Initialize the variables (i.e. assign their default value)
+init = tf.global_variables_initializer()
+
+# Start training
 with tf.Session() as sess:
-    # 初始化所有变量
-    sess.run(tf.global_variables_initializer())
 
-    pre_training_loss = 0
-    for epoch_i in range(n_epochs):
-        for (x, y) in zip(xs, ys):
+    # Run the initializer
+    sess.run(init)
+
+    # Fit all training data
+    for epoch in range(training_epochs):
+        for (x, y) in zip(train_X, train_Y):
             sess.run(optimizer, feed_dict={X: x, Y: y})
-        training_loss = sess.run(loss, feed_dict={X: xs, Y: ys})
-        print(training_loss)
-        if epoch_i % 20 == 0:
-            ax.plot(xs, Y_predict.eval(feed_dict={X: xs}), 'k', alpha=epoch_i / n_epochs)
-        #     fig.show()
-        #     plt.draw()
 
-        if np.abs(pre_training_loss - training_loss) < 0.0001:
-            break
-        pre_training_loss = training_loss
-    print(sess.run(w))
-    print(sess.run(b))
-# fig.show()
-# plt.waitforbuttonpress()
+        # Display logs per epoch step
+        if (epoch+1) % display_step == 0:
+            c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
+            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
+                "W=", sess.run(W), "b=", sess.run(b))
+
+    print("Optimization Finished!")
+    training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+    print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+
+    # Graphic display
+    plt.plot(train_X, train_Y, 'ro', label='Original data')
+    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    plt.legend()
+    plt.show()
+
+    # Testing example, as requested (Issue #2)
+    test_X = np.asarray([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1])
+    test_Y = np.asarray([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03])
+
+    print("Testing... (Mean square loss Comparison)")
+    testing_cost = sess.run(
+        tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * test_X.shape[0]),
+        feed_dict={X: test_X, Y: test_Y})  # same function as cost above
+    print("Testing cost=", testing_cost)
+    print("Absolute mean square loss difference:", abs(
+        training_cost - testing_cost))
+
+    plt.plot(test_X, test_Y, 'bo', label='Testing data')
+    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    plt.legend()
+    plt.show()
