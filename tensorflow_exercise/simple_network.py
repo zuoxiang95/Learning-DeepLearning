84a151e35dc8963afade56426bd6cb3042839581
zuoxiang <zx_data@126.com>
2017-09-24 21:51:31 +0800

    K-means in TensorFlow

---
+++ tensorflow_exercise/simple_network.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+# 模型超参数
+batch_size = 32
+epoch_num = 20000
+learning_rate = 0.01
+display_num = 500
+dropout_rate = 0.5
+
+# 加载数据
+mnist = input_data.read_data_sets(r"E:\data", one_hot=True)
+
+# 初始化输入输出占位符
+x = tf.placeholder(dtype=tf.float32, shape=[None, 784], name='input_tensor')
+y = tf.placeholder(dtype=tf.float32, shape=[None, 10], name='output_tensor')
+
+# 定义模型
+dense_1 = tf.layers.dense(inputs=x, units=256,
+                          kernel_initializer=tf.random_normal_initializer(),
+                          activation=tf.nn.sigmoid, name='dense_1')
+dense_2 = tf.layers.dense(inputs=dense_1, units=256,
+                          kernel_initializer=tf.random_normal_initializer(),
+                          activation=tf.nn.sigmoid, name='dense_2')
+output = tf.layers.dense(inputs=dense_2, units=10,
+                         kernel_initializer=tf.random_normal_initializer(),
+                         activation=tf.nn.sigmoid, name='output')
+
+# 定义损失函数
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output))
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+correct_pred = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+init_op = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    # 初始化变量
+    sess.run(init_op)
+
+    # for epoch_i in range(epoch_num):
+    #     num_steps = int(mnist.train.num_examples / batch_size)
+
+    for steps in range(epoch_num):
+        batch_x, batch_y = mnist.train.next_batch(batch_size)
+        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+
+        if steps % display_num == 0:
+            batch_x, batch_y = mnist.test.next_batch(300)
+            my_loss, batch_accuracy = sess.run([loss, accuracy], feed_dict={x: batch_x, y: batch_y})
+            print('Epoch: %d, avg_loss: %f, Accuracy: %f' % (steps, my_loss, batch_accuracy))
+
+    print("Optimization Finished!")
+
+    print("Testing Accuracy:", sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
